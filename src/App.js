71a4b71ff2968f12ea8e5dbb54dcdb02b927923f
Tinashe Muchineripi <tinashemuchineripi@gmail.com>
2021-01-19 16:57:00 +0200

    show movies on the homepage

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import GetMovies from './Components/GetMovies'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Header from './Components/Header/Header'
@@ -9,6 +10,27 @@ import WatchList from './Components/WatchList/WatchList'
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [movie, setMovie] = useState([])
+
+  function onChange(e) {
+      e.preventDefault()
+      setQuery(e.target.value)
+      const urlEncodedQuery = encodeURIComponent(query)
+      
+      fetch (`https://api.themoviedb.org/3/search/movie?api_key=2275e0d385947cc56fd79c498e925dba&page=1&include_adult=false&query=${urlEncodedQuery}`)
+          .then(response => response.json())
+          .then(data => {
+            if(!data.errors){
+              setMovie(data.results)
+            }else {
+              setMovie([])
+            }
+          })
+          .catch(err=>{console.error(err)})
+          console.log(movie)
+  }
+  
   return (
     <div className="App">
     <Router>
@@ -35,9 +57,27 @@ function App() {
       </>
       </Router>
       <form>
-        <input type='text' />
-        <button type='submit'>search</button>
+        <input 
+              type='text'
+              placeholder='Search for a movie'
+              value={query}
+              onChange={onChange} />
       </form>
+        <div>
+          {movie.length > 0 &&
+          <ul>
+            {movie.map((item, i)=>(
+              <li key={i}>
+              <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} alt={`${item.title} path`}></img>
+              <span>{item.title}</span><br />
+              <span>{item.release_date.substring(0,4)}</span><br />
+              <span>{item.overview}</span><br /> 
+              <button type="button" value="WatchList">Add to Watch List</button>
+              </li>
+            ))}
+          </ul>
+            }
+        </div>
       <GetMovies />
     </div>
     
